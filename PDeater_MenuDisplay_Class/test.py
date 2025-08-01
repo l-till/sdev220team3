c8369dcdc9877921b6038907a7a22b4f14f255f5
Paul-Deater <pdeater@ivytech.edu>
2025-07-31 23:20:36 -0400

    Add files via upload
    There shouldn't be any errors right now, but please test it. While saying that, the ability to edit the menu is still not complete.

--- PDeater_MenuDisplay_Class/test.py
+++ PDeater_MenuDisplay_Class/test.py
@@ -10,31 +10,29 @@ class OrderManager(ttk.Frame):
       self.current_table = None
       self.current_seat = None
 
-      #super().__init__(parent, borderwidth=1, relief="groove")
-      #self.pack(fill="both", expand=True)
       self.right_box = ttk.Frame()
       self.right_box.grid(row=0, column=1, sticky="nsew")
       
 
       # Storage -- Remove if broken
       self.order_storage = {
-          "Table_1": [
-              {'seat': '','name': '','price': ''}
+          "Table 1": [
+              {'seat': '','name': '','price': 0}
           ],
-          "Table_2": [
-              {'seat': '','name': '','price': ''}
+          "Table 2": [
+              {'seat': '','name': '','price': 0}
           ],
-          "Table_3": [
-              {'seat': '','name': '','price': ''}
+          "Table 3": [
+              {'seat': '','name': '','price': 0}
           ],
-          "Table_4": [
-              {'seat': '','name': '','price': ''}
+          "Table 4": [
+              {'seat': '','name': '','price': 0}
           ],
-          "Booth_1": [
-              {'seat': '','name': '','price': ''}
+          "Booth 1": [
+              {'seat': '','name': '','price':0}
           ],
-          "Booth_2": [
-              {'seat': '','name': '','price': ''}
+          "Booth 2": [
+              {'seat': '','name': '','price': 0}
           ],
         }
       
@@ -59,22 +57,46 @@ class OrderManager(ttk.Frame):
       self.place_order_button = Button(self.right_box, text="Place Order", command=self.place_order)
       self.place_order_button.grid(row=3, column=0, pady=10)
 
-      self.total = 0.0
+      self.table_label = Label(self.right_box, text=f"Active Table: {self.current_table}")
+      self.table_label.grid(row=4, column=0, pady=5)
 
+      self.seat_label = Label(self.right_box, text='')
+      self.seat_label.grid(row=5, column=0, pady=5)
+
+      self.total = 0.0
 
     #Paul Additions
    def set_table_or_booth(self, table_or_booth):
-      self.current_table = table_or_booth
-      self.table_label = Label(self.right_box, text=f"Active Table: {self.current_table}")
-      self.table_label.grid(row=4, column=0, pady=5)
+      if table_or_booth != self.current_table:
+        self.current_table = table_or_booth
+
+        if table_or_booth in self.order_storage:
+          self.tree.delete(*self.tree.get_children())
+
+          self.active_order_storage = self.order_storage[table_or_booth]
+          self.total = 0.0
+
+          for main_key, sub_key in enumerate(self.active_order_storage):
+            #This check exists because I want order_storage to have examples of its format
+            if sub_key['price'] != 0:
+                self.tree.insert("", "end", values=(sub_key['seat'], sub_key['name'], f"${sub_key['price']}"))
+
+            self.total += sub_key['price']
+            self.total_label.config(text=f"Total: ${self.total:.2f}")
+
+
+        self.table_label.config(text=f"Active Table: {self.current_table}")
+        self.current_seat = ""
+        self.seat_label.config(text="No Active Seat")
+      else:
+          pass
 
    def set_current_seat(self,table_or_booth,seat_number,select_or_deselect):
       if table_or_booth == self.current_table and select_or_deselect:
 
         self.current_seat = seat_number
 
-        self.seat_label = Label(self.right_box, text=f"Active Seat: {self.current_seat}")
-        self.seat_label.grid(row=5, column=0, pady=5)
+        self.seat_label.config(text=f"Active Seat: {self.current_seat}")
       elif select_or_deselect == False:
           self.current_seat = ""
           self.seat_label.config(text="No Active Seat")
@@ -86,13 +108,15 @@ class OrderManager(ttk.Frame):
         self.tree.insert("", "end", values=(self.current_seat, name, f"${price:.2f}"))
         self.total += price
         self.total_label.config(text=f"Total: ${self.total:.2f}")
+
+        self.active_order_storage.append({'seat': self.current_seat,'name': name,'price': price})
+
       else:
           messagebox.showwarning(title="ERROR - No Table Selected", message="Please select a Table")
 
    def place_order(self):
       self.order_status.set("Order placed successfully!")
       messagebox.showinfo("Order Status", "Your order has been placed.")
-
 
 
 class MenuDisplay:
@@ -164,6 +188,7 @@ class MenuDisplay:
          {'name': 'Sea Bass', 'price': 24.49},
          ],
       }
+        
     """
     Creates the menu items within a frame, along with a button to modify the menu.
     Requires three values, the first two are for position and the third is for determining manager access.
@@ -175,7 +200,6 @@ class MenuDisplay:
         # Creates the base menu frame
         self.menu_frame = tk.Frame(self.root, bg="gray85",width=500, height=100)
         self.menu_frame.grid(row = row_position, column = column_position)
-        #Ignore # menu_frame.grid_propagate(0) #Ignore
         
         # Creates a frame to hold buttons for choosing sections
         section_frame = tk.Frame(self.menu_frame, bg="gray85")
@@ -401,30 +425,23 @@ class Diningapp(ttk.Frame):
                     self.seat_vars[(table_name, i+1)] = var
 
     def select_table(self, table_name):
-        
-        #btn = self.tables[table_name]
-        #btn.configure(style="Selected.TButton")
         self.on_table_select.set_table_or_booth(table_name)
 
     def toggle_seat(self, table_name, seat_number, var):
         if var.get():
             if seat_number not in self.selected_seats[table_name]:
                 self.selected_seats[table_name].append(seat_number)
-                print(f"{table_name} - Seat {seat_number} selected.")
+                #print(f"{table_name} - Seat {seat_number} selected.")
                 self.on_table_select.set_current_seat(table_name,seat_number,True)
                 #if self.on_table_select:
                  #   self.on_table_select(table_name=table_name, seat_number=seat_number, selected=True)
         else:
             if seat_number in self.selected_seats[table_name]:
                 self.selected_seats[table_name].remove(seat_number)
-                print(f"{table_name} - Seat {seat_number} deselected.")
+                #print(f"{table_name} - Seat {seat_number} deselected.")
                 self.on_table_select.set_current_seat(table_name,seat_number,False)
                 #if self.on_table_select:
                  #   self.on_table_select(table_name=table_name, seat_number=seat_number, selected=False)
-
-
-
-
 
 
 class LoginScreen(ttk.Frame):
@@ -450,7 +467,6 @@ class LoginScreen(ttk.Frame):
       pad_frame = ttk.Frame(self)
       pad_frame.pack()
     
-
       buttons = [
          ('1', '2', '3'),
          ('4', '5', '6'),
@@ -499,21 +515,12 @@ def you_logged_in(user_info):
     for widget in root.winfo_children():
         widget.destroy()
 
-
-    def handle_selection(table_name, seat_number, selected):
-        pass
-        #print(f"[OrderManager] Table: {table_name}, Seat: {seat_number}, Selected: {selected}")
-
-
     OrderManager()
     OrderManager_object = OrderManager()
 
 
     frame = Diningapp(root, OrderManager_object)
     frame.grid(row=0,column=0,padx=10,pady=10)
-
-
-
 
 
     x = MenuDisplay(root, OrderManager_object)
@@ -525,11 +532,10 @@ def you_logged_in(user_info):
 
     if user_info[2]:
         tk.Label(user_info_frame,text= f"** Manager View **", font=("Arial", 16)).pack(pady=5)
+
     tk.Label(user_info_frame,text= f"Name - {user_info[1]}", font=("Arial", 16)).pack(pady=5)
     tk.Label(user_info_frame,text= f"User ID - {user_info[0]}", font=("Arial", 16)).pack(pady=5)
     
-
-
 
 def clear_window():
     for widget in root.winfo_children():
